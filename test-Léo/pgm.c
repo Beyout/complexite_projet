18b8dedffcc7f6a6dc60cfec50523a4ec1c811b1
Flodeco22 <flodeco22@outlook.fr>
2021-02-17 12:23:57 +0100

    Fichier test V2

--- test-Léo/pgm.c
+++ test-Léo/pgm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "pgm.h"
  
 /**********************File I/O functions*******************************/
@@ -27,6 +28,7 @@ void getPGMfile (char filename[], PGMImage *img)
    {
      printf("ERROR(1): Not valid pgm/ppm file type\n");
      exit(1);
+     getc(in_file);
    }
    ch = getc(in_file);
    /*convert the one digit integer currently represented as a character to
@@ -38,21 +40,46 @@ void getPGMfile (char filename[], PGMImage *img)
       exit(1);
    }
     int ligne  = getc(in_file);
-    while(ligne!=-1){           /* skip to end of line*/
-	if((char) ligne == '\n'){
+    int nbN = 0;
+    while(nbN<3){
+	if(ligne==10){
+		nbN++;
+	}
+	ligne  = getc(in_file);
+    }
+
+    //ligne  = getc(in_file);
+    int temps =0;
+    int longueur =0;
+    while(!feof( in_file )){
+	if(longueur==512){
 		printf("\n");
+		printf("longueur -> %d\n", longueur);
+		if(temps==1){		
+			getchar();
+		}
+		temps=0;
+		longueur=0;
 	}
 	else{	
 		if(ligne<50){
-			printf("o");
+			printf("x");
+			temps=1;
 		}
 		else{
-			printf("x");
+			printf("o");
 		}
 	}
+	longueur++;
+	//printf("%d", ligne);
+	//printf("\n");
+	//sleep(1);
 	printf(" ");
+
 	ligne  = getc(in_file);
     }
+    printf("\n");
+    printf("longueur -> %d\n", longueur);
   
     fclose(in_file);
     printf("\nDone reading file.\n");
@@ -60,7 +87,7 @@ void getPGMfile (char filename[], PGMImage *img)
 
 int main(void){
 	PGMImage *img = malloc(sizeof(PGMImage));
-	getPGMfile ("radius_2.pgm", img);
+	getPGMfile ("radius_4.pgm", img);
 	free(img);
 	return 0;
 }
